b8081af9a49303ee4a8a907c8bc79ac36dd38818
eoieiie <turkiyecivciv31@gmail.com>
2024-07-17 19:38:47 +0900

    restart

--- lib/section/home/viewmodel/home_viewmodel.dart
+++
@@ -1,7 +0,0 @@
-// home_viewmodel.dart
-import 'package:get/get.dart';
-
-class HomeViewModel extends GetxController {
-  // HomeViewModel 관련 로직
-}
-
--- lib/section/home/viewmodel/search_viewmodel.dart
+++
@@ -1,7 +0,0 @@
-// search_viewmodel.dart
-import 'package:get/get.dart';
-
-class SearchViewModel extends GetxController {
-  // SearchViewModel 관련 로직
-}
-
